c49f5496aef4286e2f3653d20fefbfba99015a57
elhcs <elhoussaine.chahboun@gmail.com>
2024-06-16 22:41:59 +0100

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,61 +1,45 @@
 import React, { useEffect } from "react";
 import "./Navbar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import setAuthToken from "../utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "../reducers/authReducers";
 import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  const { isAuthenticated, userInfo, userToken } = useSelector(
-    (state) => state.auth
-  );
+  const { isAuthenticated } = useSelector((state) => state.auth);
   const { light } = useSelector((state) => state.light);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  //using the navbar to manage token
+  const location = useLocation(); // Use location to check the current route
+
   useEffect(() => {
-    if (userToken) {
-      setAuthToken(userToken);
-      const decoded = jwt_decode(userToken);
-      dispatch(
-        setCurrentUser({
-          token: userToken,
-          success: decoded.success,
-          userdata: {
-            id: decoded.id,
-            name: decoded.name,
-            isAdmin: decoded.isAdmin,
-            email: decoded.email,
-          },
-        })
-      );
-      // Check for expired token
-      const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (localStorage.jwtToken) {
+      setAuthToken(localStorage.jwtToken);
+      const decoded = jwt_decode(localStorage.jwtToken);
+      dispatch(setCurrentUser(decoded));
+
+      const currentTime = Date.now() / 1000;
       if (decoded.exp < currentTime) {
-        // Logout user
         dispatch(logoutUser());
-        // Redirect to login
         navigate("/login");
       }
     }
-  }, [userToken]);
+  }, [dispatch, navigate]);
+
+  // Determine if the current page is the About page
+  const isAboutPage = location.pathname === "/about";
 
   return (
     <nav
       className="navbar navbar-expand-lg navbar-light"
       style={{
-        backgroundColor: light ? "black" : "rgba(0, 0, 0, 0)",
+        backgroundColor: isAboutPage ? "white" : "black",
+        borderBottom: isAboutPage ? "3px solid white" : "none"
       }}
     >
       <div className="container-fluid">
-        <NavLink
-          to="/"
-          id="LogoText"
-          className="navbar-brand"
-          style={{ color: light ? "white" : "white" }}
-        >
+        <NavLink to="/" id="LogoText" className="navbar-brand" style={{ color: isAboutPage ? "black" : "white" }}>
           EL HOUSSAINE
         </NavLink>
         <button
@@ -67,50 +51,28 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon "></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul className="navbar-nav d-flex ms-auto nav-list">
-          <li className="nav-item ms-auto p-2">
-              <NavLink
-                to="/about"
-                className="nav-link active"
-                aria-current="page"
-                style={{ color: light ? "white" : "white" }}
-              >
+          <ul className={`navbar-nav d-flex ms-auto nav-list ${isAboutPage ? "special-nav-list-class" : ""}`}>
+            <li className={`nav-item ms-auto p-2 ${isAboutPage ? "special-about-item" : ""}`}>
+              <NavLink to="/about" className={`nav-link ${isAboutPage ? "special-about-class" : ""}`} aria-current="page" style={{ color: isAboutPage ? "black" : "white" }}>
                 ABOUT
               </NavLink>
-              </li>
-            <li className="nav-item ms-auto p-2">
-              <NavLink
-                to="/admin"
-                className="nav-link active"
-                style={{ color: light ? "white" : "white" }}
-              >
+            </li>
+            <li className={`nav-item ms-auto p-2 ${isAboutPage ? "special-about-item" : ""}`}>
+              <NavLink to="/admin" className="nav-link" style={{ color: isAboutPage ? "black" : "white" }}>
                 PORTFOLIO
               </NavLink>
             </li>
-            <li
-              className="nav-item ms-auto p-2"
-              onClick={() => {
-                window.location.replace("/#footer_section_id");
-              }}
-            >
-              <NavLink
-                to="/"
-                className="nav-link active"
-                style={{ color: light ? "white" : "white" }}
-              >
+            <li className={`nav-item ms-auto p-2 ${isAboutPage ? "special-about-item" : ""}`}>
+              <NavLink to="/" className="nav-link" style={{ color: isAboutPage ? "black" : "white" }}>
                 CONTACT
               </NavLink>
             </li>
             {!isAuthenticated && (
-              <li className="nav-item ms-auto p-2">
-                <NavLink
-                  to="/login"
-                  className="nav-link active"
-                  style={{ color: light ? "white" : "white" }}
-                >
+              <li className={`nav-item ms-auto p-2 ${isAboutPage ? "special-about-item" : ""}`}>
+                <NavLink to="/login" className="nav-link" style={{ color: isAboutPage ? "black" : "white" }}>
                   LOGIN
                 </NavLink>
               </li>
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -40,38 +40,7 @@ const MyComponent = () => {
 
     
     <div>
-      
-      {/* Two additional columns */}
-      <div className={styles.additionalColumnsContainer}>
-        <div className={styles.additionalColumnLeft} style={{ width: '85%' }}>
-          {/* Content for left column */} 
-          <div className={styles.bio} >
-            <div className={styles.biotitle} >{'ABOUT ME'}
-            </div>
-          </div>
-        </div>
-        <div className={styles.additionalColumnRight} style={{ width: '15%' }}>
-          {/* Content for right column */} 
-          <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }}
-          >
-            <div className={styles.rightArrow}>
-            <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="214"
-          height="214"
-          viewBox="0 0 224 224"
-          fill="none"
-        >
-          <rect width="214" height="214" fill="none" />
-          <path
-            d="M109.379 157.621C110.55 158.793 112.45 158.793 113.621 157.621L132.713 138.529C133.885 137.358 133.885 135.458 132.713 134.287C131.542 133.115 129.642 133.115 128.471 134.287L111.5 151.257L94.5294 134.287C93.3579 133.115 91.4584 133.115 90.2868 134.287C89.1152 135.458 89.1152 137.358 90.2868 138.529L109.379 157.621ZM108.5 70V155.5H114.5V70H108.5Z"
-            fill="white"
-          />
-        </svg>
-    </div>
-          </div>
-        </div>
-      </div>
+     
       <div style={{ width: '100%', height: '30px', backgroundColor: 'white' }}
           ></div>
       <div style={{ display: 'flex' }}>
@@ -89,7 +58,10 @@ const MyComponent = () => {
       </div>
     </div>
   </div>
-  <div style={{ overflowY: 'auto', maxHeight: '640px', padding: '1%'}}>
+  
+</div>
+
+<div style={{ overflowY: 'auto', maxHeight: '640px', padding: '1%'}}>
     {/* Add your scrollable content here */}
     <h3 style={{ fontSize: '40px' }} className={styles.biotitle}></h3>
     <p className={styles.cardDescription}> </p>
@@ -100,9 +72,6 @@ const MyComponent = () => {
 In hac habitasse platea dictumst. In vulputate hendrerit tincidunt. Erat. Cras rutrum pharetra risus vitae ullamcorper. Donec non convallis erat. Praesent aliquet, lacus nec sollicitudin varius, diam enim molestie lorem, eu viverra nulla nulla tincidunt mi. Cras porta tincidunt nisl vel vulputate. Suspendisse a massa pharetra, commodo urna at, posuere urna. Proin faucibus convallis turpis, et fermentum justo dignissim non. Cras quis sem quis justo ultricies viverra sed porta quam. Nullam nec eros id nulla auctor lacinia.
 
  tincidunt. </p></div>
-</div>
-
-    
 
     <Footer />
     </div>
